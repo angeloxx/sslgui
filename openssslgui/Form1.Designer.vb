929b685a950817c9e9a111592335577556bf2756
angeloxx <angeloxx@angeloxx.it>
2018-04-14 19:44:53 +0200

    initial commit

---
+++ openssslgui/Form1.Designer.vb
@@ -0,0 +1,188 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.txtDecodeIn = New System.Windows.Forms.TextBox()
+        Me.txtDecodeOut = New System.Windows.Forms.TextBox()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.lblStatusBar = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.txtNodesIn = New System.Windows.Forms.TextBox()
+        Me.txtNodesOut = New System.Windows.Forms.TextBox()
+        Me.txtNodesPass = New System.Windows.Forms.TextBox()
+        Me.lblNodesPassword = New System.Windows.Forms.Label()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Location = New System.Drawing.Point(12, 12)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(702, 444)
+        Me.TabControl1.TabIndex = 0
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.txtDecodeIn)
+        Me.TabPage1.Controls.Add(Me.txtDecodeOut)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(694, 418)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "DecodeX509"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'txtDecodeIn
+        '
+        Me.txtDecodeIn.Font = New System.Drawing.Font("Consolas", 8.830189!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtDecodeIn.Location = New System.Drawing.Point(6, 6)
+        Me.txtDecodeIn.Multiline = True
+        Me.txtDecodeIn.Name = "txtDecodeIn"
+        Me.txtDecodeIn.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtDecodeIn.Size = New System.Drawing.Size(682, 93)
+        Me.txtDecodeIn.TabIndex = 1
+        '
+        'txtDecodeOut
+        '
+        Me.txtDecodeOut.Font = New System.Drawing.Font("Consolas", 8.830189!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtDecodeOut.Location = New System.Drawing.Point(6, 105)
+        Me.txtDecodeOut.Multiline = True
+        Me.txtDecodeOut.Name = "txtDecodeOut"
+        Me.txtDecodeOut.ReadOnly = True
+        Me.txtDecodeOut.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtDecodeOut.Size = New System.Drawing.Size(682, 307)
+        Me.txtDecodeOut.TabIndex = 0
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.lblNodesPassword)
+        Me.TabPage2.Controls.Add(Me.txtNodesPass)
+        Me.TabPage2.Controls.Add(Me.txtNodesIn)
+        Me.TabPage2.Controls.Add(Me.txtNodesOut)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(694, 418)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "NoDES"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.ImageScalingSize = New System.Drawing.Size(18, 18)
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.lblStatusBar})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 459)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(726, 22)
+        Me.StatusStrip1.TabIndex = 1
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'lblStatusBar
+        '
+        Me.lblStatusBar.Name = "lblStatusBar"
+        Me.lblStatusBar.Size = New System.Drawing.Size(0, 17)
+        '
+        'txtNodesIn
+        '
+        Me.txtNodesIn.Font = New System.Drawing.Font("Consolas", 8.830189!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNodesIn.Location = New System.Drawing.Point(6, 6)
+        Me.txtNodesIn.Multiline = True
+        Me.txtNodesIn.Name = "txtNodesIn"
+        Me.txtNodesIn.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtNodesIn.Size = New System.Drawing.Size(682, 93)
+        Me.txtNodesIn.TabIndex = 3
+        '
+        'txtNodesOut
+        '
+        Me.txtNodesOut.Font = New System.Drawing.Font("Consolas", 8.830189!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNodesOut.Location = New System.Drawing.Point(6, 140)
+        Me.txtNodesOut.Multiline = True
+        Me.txtNodesOut.Name = "txtNodesOut"
+        Me.txtNodesOut.ReadOnly = True
+        Me.txtNodesOut.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtNodesOut.Size = New System.Drawing.Size(682, 272)
+        Me.txtNodesOut.TabIndex = 2
+        '
+        'txtNodesPass
+        '
+        Me.txtNodesPass.Location = New System.Drawing.Point(73, 109)
+        Me.txtNodesPass.Name = "txtNodesPass"
+        Me.txtNodesPass.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.txtNodesPass.Size = New System.Drawing.Size(615, 20)
+        Me.txtNodesPass.TabIndex = 4
+        '
+        'lblNodesPassword
+        '
+        Me.lblNodesPassword.AutoSize = True
+        Me.lblNodesPassword.Location = New System.Drawing.Point(6, 112)
+        Me.lblNodesPassword.Name = "lblNodesPassword"
+        Me.lblNodesPassword.Size = New System.Drawing.Size(61, 15)
+        Me.lblNodesPassword.TabIndex = 5
+        Me.lblNodesPassword.Text = "Password"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(726, 481)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.TabControl1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "frmMain"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "SSLGui"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage2.PerformLayout()
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents txtDecodeIn As TextBox
+    Friend WithEvents txtDecodeOut As TextBox
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents lblStatusBar As ToolStripStatusLabel
+    Friend WithEvents txtNodesIn As TextBox
+    Friend WithEvents txtNodesOut As TextBox
+    Friend WithEvents lblNodesPassword As Label
+    Friend WithEvents txtNodesPass As TextBox
+End Class
---
+++ openssslgui/Form1.vb
@@ -0,0 +1,59 @@
+﻿Imports System.IO
+Imports System.Security.Cryptography.X509Certificates
+Imports System.Text
+Public Class frmMain
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtDecodeIn.AllowDrop = True
+        lblStatusBar.Text = "Version " + Application.ProductVersion
+    End Sub
+
+    Private Sub txtDecodeIn_DragDrop(sender As Object, e As DragEventArgs) Handles txtDecodeIn.DragDrop
+        Dim files() As String = e.Data.GetData(DataFormats.FileDrop)
+
+        If files.Count = 1 Then
+            Dim infile = files.GetValue(0)
+            If infile.EndsWith(".crt") Or infile.EndsWith(".pem") Then
+                txtDecodeOut.Text = ""
+                txtDecodeIn.Text = File.ReadAllText(infile)
+                Decode()
+            End If
+        End If
+    End Sub
+
+    Private Sub txtDecodeIn_DragEnter(sender As Object, e As DragEventArgs) Handles txtDecodeIn.DragEnter
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            e.Effect = DragDropEffects.Copy
+        End If
+    End Sub
+
+    Private Sub Decode()
+        txtDecodeOut.Text = ""
+        Try
+
+            File.WriteAllText(Path.GetTempPath() + "opensslgui-in.txt", txtDecodeIn.Text)
+            Dim Cert As X509Certificate2 = New X509Certificate2(X509Certificate.CreateFromCertFile(Path.GetTempPath() + "opensslgui-in.txt"))
+
+            Dim SN = String.Join(":", Cert.GetSerialNumberString().Select(Function(x, n) New With {x, n}).GroupBy(Function(x) x.n \ 2, Function(x) x.x).Select(Function(y) New String(y.ToArray())))
+            txtDecodeOut.Text += "Name: " + Cert.Subject + vbNewLine
+            'txtDecodeOut.Text += "SAN: " +
+            txtDecodeOut.Text += "Issuer: " + Cert.Issuer + vbNewLine
+            txtDecodeOut.Text += "Serial Number: " + SN + vbNewLine
+            txtDecodeOut.Text += "Valid From: " + Cert.GetEffectiveDateString + vbNewLine
+            txtDecodeOut.Text += "Valid To: " + Cert.GetExpirationDateString + vbNewLine
+            For Each extension As X509Extension In Cert.Extensions
+                If extension.Oid.FriendlyName.ToString = "Subject Alternative Name" Then
+                    txtDecodeOut.Text += "== SAN ==" + vbNewLine
+                    txtDecodeOut.Text += extension.Format(True)
+                End If
+            Next
+
+
+
+
+            'txtDecodeOut.Text = Cert.ToString(True)
+        Catch ex As Exception
+            txtDecodeOut.Text = "[invalid element]"
+        End Try
+
+    End Sub
+End Class
